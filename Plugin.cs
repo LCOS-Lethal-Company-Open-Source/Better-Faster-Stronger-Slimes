ce11d3f20059c2caf46bfd9ee342451b8d890660
BastedEggsRYummy <baimej@rpi.edu>
2024-04-02 16:24:43 -0400

    Changed filenames and implemented logging

--- Plugin.cs
+++ Plugin.cs
@@ -1,5 +1,10 @@
-﻿using BepInEx;
-namespace Template;
+﻿using System;
+using BepInEx;
+using BepInEx.Logging;
+using GameNetcodeStuff;
+using HarmonyLib;
+using UnityEngine;
+namespace betterFasterStrongerSlimes;
 
 [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 public class Plugin : BaseUnityPlugin
@@ -8,19 +13,21 @@ public class Plugin : BaseUnityPlugin
   {
     // Plugin load logic goes here!
     // This script acts like a unity object.
-    Logger.LogInfo($"Hello World!");
+    Logger.LogInfo($"Better Faster Stronger Slimes Active!");
   }
 
   [HarmonyPatch(typeof(BlobAI), "Start")]
 
-  class startingCash
+  class myClass
   {
     private static void Postfix(ref BlobAI __instance)
     {
+      ManualLogSource mls = BepInEx.Logging.Logger.CreateLogSource(PluginInfo.PLUGIN_GUID);
       for(int i = 0; i < __instance.maxDistanceForSlimeRays.Length; i++)
       {
-        __instance.maxDistanceForSlimeRays[i] = 9f;
-        __instance.SlimeBonePositions[i] = __instance.SlimeBones[i].tranform.position;
+        //__instance.maxDistanceForSlimeRays[i] = 9f;
+        //__instance.SlimeBonePositions[i] = __instance.SlimeBones[i].tranform.position;
+        mls.LogInfo($"In the loop");
       }
     }
   }
